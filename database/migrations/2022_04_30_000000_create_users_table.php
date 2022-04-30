ad79354379db7732665cb8226814f2e1b6df5ee0
kikiope Oyeniran <kikioyeniran@gmail.com>
2022-04-30 14:57:34 +0100

    Models and migrations added

--- database/migrations/2022_04_30_000000_create_users_table.php
+++ database/migrations/2022_04_30_000000_create_users_table.php
@@ -15,10 +15,14 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('employee_id');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->unsignedBigInteger('role_id');
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
             $table->rememberToken();
             $table->timestamps();
         });
